b4a2e9de2c776c0c6a1c34ef6185d7b37ff2db0e
Jack Chen <jackczy1209@gmail.com>
2023-03-28 11:44:58 -0400

    added functions list for home screen

---
+++ backend/item.js
@@ -0,0 +1,25 @@
+/* 
+this file contains all functions that interact with items
+*/
+
+
+const addToSavedItem = (user, item) => {
+
+}
+
+const removeFromSavedItem = (user, item) => {
+
+}
+
+
+const refreshFeed = (user, location) => {
+
+}
+
+
+
+export {
+    addToSavedItem,
+    removeFromSavedItem,
+    refreshFeed
+}
